b75c07639f08f4f82554e290b4e53c9b5eeb468d
linsimon <linsimon624@outlook.com>
2020-12-04 17:11:41 +0800

    write a discard server

--- src/main/java/com/example/netty/learning/App.java
+++ src/main/java/com/example/netty/learning/App.java
@@ -1,13 +1,19 @@
 package com.example.netty.learning;
 
+import com.example.netty.learning.discard.DiscardServer;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception{
+    	int port = 9090;
+    	if (args.length > 0) {
+    		port = Integer.parseInt(args[0]);
+    	}
+    	
+    	new DiscardServer(port).run();
     }
 }
---
+++ src/main/java/com/example/netty/learning/discard/DiscardServerHandler.java
@@ -0,0 +1,40 @@
+package com.example.netty.learning.discard;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * Handles a server-side channel
+ * @author simon
+ *
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter{
+
+	// Usually, channelRead() handler method is implemented like the following
+//	@Override
+//	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//		try {
+//			// Do something with msg
+//		} finally {
+//			ReferenceCountUtil.release(msg);
+//		}
+//	}
+	
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//		super.channelRead(ctx, msg);
+		//Discard the received data silently.
+		((ByteBuf)msg).release();
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+//		super.exceptionCaught(ctx, cause);
+		// Close the connection when an exception is raised.
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+}
